ddacf5adbe9dc76729b310044b519f4dd3edf0ef
Souvik Mondal <sm6984767@gmail.com>
2024-12-26 01:01:34 +0530

    some changesin request order page

--- components/request-order-page.js
+++ components/request-order-page.js
@@ -68,7 +68,7 @@ export default function RequestOrderPage() {
           </p>
           </div>
           <motion.div
-          initial={{ opacity: 0, x:-500 }}
+          initial={{ opacity: 0, x:-100 }}
           whileInView={{
             opacity: 1 ,
             x:0,
