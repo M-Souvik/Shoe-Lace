af2ec84f6ec9c0746e534121e7a7c8892b532c5e
Souvik Mondal <sm6984767@gmail.com>
2024-12-27 14:26:19 +0530

    changes in footer

--- components/footer.js
+++ components/footer.js
@@ -18,7 +18,7 @@ const Footer = () => {
             
             <div className="mt-6 md:mx-3 shrink-0 md:mt-0 md:w-auto flex flex-col sm:flex-row gap-3">
             <input type="text" placeholder="Enter your email address" className="input input-bordered sm:w-96" required/>
-            <button className="btn px-10 bg-yellow-500 hover:bg-orange-400 border-none text-white ">Subcribe</button>
+            <button className="btn px-10 bg-orange-500 hover:bg-orange-400 border-none text-white ">Subcribe</button>
             </div>
 
         </div>
@@ -29,48 +29,48 @@ const Footer = () => {
                 <Image src={'/images/white-shoes-logo.png'} width={280} height={280} alt='logo' className='py-5 sm:py-0'/>
                 <p className='text-white py-4 sm:pr-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam delectus optio animi dignissimos doloribus reiciendis</p>
                 <div className="flex items-center gap-3 pt-3">
-                    <button className='bg-yellow-500 text-white btn border-none'><LuFacebook size={20}/></button>
-                    <button className='bg-yellow-500 text-white btn border-none'><LuTwitter size={20}/></button>
-                    <button className='bg-yellow-500 text-white btn border-none'><LuInstagram size={20}/></button>
-                    <button className='bg-yellow-500 text-white btn border-none'><LuYoutube size={20}/></button>
+                    <button className='bg-orange-500 text-white btn border-none'><LuFacebook size={20}/></button>
+                    <button className='bg-orange-500 text-white btn border-none'><LuTwitter size={20}/></button>
+                    <button className='bg-orange-500 text-white btn border-none'><LuInstagram size={20}/></button>
+                    <button className='bg-orange-500 text-white btn border-none'><LuYoutube size={20}/></button>
                 </div>
             </div>
             <div className='col-span-2'>
             <div className='grid sm:grid-cols-3 gap-2'>
             <div className="pb-10">
                 <p className="font-semibold  text-2xl pb-5">Quick Links</p>
-                <hr className='w-20 h-1 border-0 rounded bg-yellow-400'/>
+                <hr className='w-20 h-1 border-0 rounded bg-orange-400'/>
 
                 <div className="flex flex-col items-start mt-5 space-y-2">
-                    <Link href="#" className=" transition-colors duration-300  hover:text-yellow-400  ">About Us</Link>
-                    <Link href="#" className=" transition-colors duration-300  hover:text-yellow-400  ">Services</Link>
-                    <Link href="#" className=" transition-colors duration-300  hover:text-yellow-400  ">Appointment</Link>
-                    <Link href="#" className=" transition-colors duration-300  hover:text-yellow-400  ">Pricing</Link>
-                    <Link href="#" className=" transition-colors duration-300  hover:text-yellow-400  ">Contact</Link>
+                    <Link href="#" className=" transition-colors duration-300  hover:text-orange-400  ">About Us</Link>
+                    <Link href="#" className=" transition-colors duration-300  hover:text-orange-400  ">Services</Link>
+                    <Link href="#" className=" transition-colors duration-300  hover:text-orange-400  ">Appointment</Link>
+                    <Link href="#" className=" transition-colors duration-300  hover:text-orange-400  ">Pricing</Link>
+                    <Link href="#" className=" transition-colors duration-300  hover:text-orange-400  ">Contact</Link>
                 </div>
             </div>
 
             <div className="pb-10">
                 <p className="font-semibold  text-2xl pb-5">Useful Links</p>
-                <hr className='w-20 h-1 border-0 rounded bg-yellow-400'/>
+                <hr className='w-20 h-1 border-0 rounded bg-orange-400'/>
 
                 <div className="flex flex-col items-start mt-5 space-y-2">
-                    <Link href="#" className=" transition-colors duration-300  hover:text-yellow-400  ">Privacy Policy</Link>
-                    <Link href="#" className=" transition-colors duration-300  hover:text-yellow-400  ">Terms and Conditions</Link>
-                    <Link href="#" className=" transition-colors duration-300  hover:text-yellow-400  ">Disclaimer</Link>
-                    <Link href="#" className=" transition-colors duration-300  hover:text-yellow-400  ">Support</Link>
-                    <Link href="#" className=" transition-colors duration-300  hover:text-yellow-400  ">FAQ</Link>
+                    <Link href="#" className=" transition-colors duration-300  hover:text-orange-400  ">Privacy Policy</Link>
+                    <Link href="#" className=" transition-colors duration-300  hover:text-orange-400  ">Terms and Conditions</Link>
+                    <Link href="#" className=" transition-colors duration-300  hover:text-orange-400  ">Disclaimer</Link>
+                    <Link href="#" className=" transition-colors duration-300  hover:text-orange-400  ">Support</Link>
+                    <Link href="#" className=" transition-colors duration-300  hover:text-orange-400  ">FAQ</Link>
                 </div>
             </div>
 
             <div className="pb-10 sm:pb-0">
                 <p className="font-semibold  text-2xl pb-5">Work Hours</p>
-                <hr className='w-20 h-1 border-0 rounded bg-yellow-400'/>
+                <hr className='w-20 h-1 border-0 rounded bg-orange-400'/>
 
                 <div className="flex flex-col items-start mt-5 space-y-4">
-                    <span className='flex gap-2 items-center'><LuClock8 className='text-yellow-400'/><h1 className='text-white'>9 AM - 10 AM, Monday - Saturday</h1></span>
+                    <span className='flex gap-2 items-center'><LuClock8 className='text-orange-400'/><h1 className='text-white'>9 AM - 10 AM, Monday - Saturday</h1></span>
                     <p className='text-white'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae perferendis quos facilis error ullam quidem facere eveniet cumque unde velit!</p>
-                    <button className="btn px-10 bg-yellow-500 hover:bg-orange-400 border-none text-white font-medium">Order Pickup Now</button>
+                    <button className="btn px-10 bg-orange-500 hover:bg-orange-400 border-none text-white font-medium">Order Pickup Now</button>
                 </div>
             </div>
 
